f1849ed266afd285b1f6ab95a255202af2f1519b
Elena <elenamelcan@gmail.com>
2024-10-11 11:25:22 -0400

    Initial Commit

---
+++ iOSShowCase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSShowCase
+//
+//  Created by Elena Melnikova on 2024-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("iOS showcase")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSShowCase/iOSShowCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSShowCaseApp.swift
+//  iOSShowCase
+//
+//  Created by Elena Melnikova on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct iOSShowCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
